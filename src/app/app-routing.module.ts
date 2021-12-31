62c287b3087ce980f1c16a1a9941adf7fc0030cf
bastien <method1990@gmail.com>
2021-12-31 22:55:32 +0100

    add menu with navigation for every view but login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
 const routes: Routes = [
+  {
+    path: '', component: MenuComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+      },
+      {
+        path: 'mathematics',
+        loadChildren: () => import('./mathematics/mathematics.module').then((m) => m.MathematicsModule)
+      }
+    ]
+  },
   // { path: '', component: WrapperWithMenu, children: [...]}
   {
     path: 'user',
     loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
-  },
-  {
-    path: 'mathematics',
-    loadChildren: () => import('./mathematics/mathematics.module').then((m) => m.MathematicsModule)
   },
   {
     path: '**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [AppComponent, MenuComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule, MatIconModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
